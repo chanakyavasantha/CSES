6c2de6b71f2533981caf3b4ba0e69e344c7d4841
chanakya <chanakyavasantha@gmail.com>
2023-10-25 20:08:44 +0530

    q1

---
+++ Introduction/weirdalgorithm.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    long long n;
+    cin>>n;
+    while( n != 1){
+        if(n%2){
+            cout<<n<<" ";
+            n *= 3;
+            n += 1;
+        }
+        else{
+            cout<<n<<" ";
+            n /= 2;
+        }
+    }
+    cout<<1<<" ";
+}
