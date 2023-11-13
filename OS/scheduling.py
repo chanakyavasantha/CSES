790b1a92d6572c8d44fb4cdeef0fa1cd96246a14
chanakya <chanakyavasantha@gmail.com>
2023-11-13 23:06:08 +0530

    update

--- OS/scheduling.py
+++ OS/scheduling.py
@@ -25,24 +25,26 @@ def priority_preemption_schedule(tasks):
     tasks.sort(key=lambda x: (x[1], x[2]))  # Sort by priority and burst time
     return [task[0] for task in tasks]
 
-def srtf_schedule(tasks):
-    time = 0
+def priority_round_robin_schedule(tasks, time_quantum):
     schedule = []
-    while tasks:
-        eligible_tasks = [task for task in tasks if task[1] <= time]
-        if eligible_tasks:
-            shortest_task = min(eligible_tasks, key=lambda x: x[2])
-            schedule.append(shortest_task[0])
-            time += shortest_task[2]
-            tasks.remove(shortest_task)
-        else:
-            time += 1
+    queue = tasks.copy()
+    queue.sort(key=lambda x: x[1])  # Sort by priority initially
+
+    while queue:
+        task = queue.pop(0)
+        schedule.append(task[0])
+        task[2] -= time_quantum
+
+        if task[2] > 0:
+            queue.append(task)
+            queue.sort(key=lambda x: x[1])  # Re-sort by priority after preemption
+
     return schedule
 
 tasks = [["Task1", 3, 5], ["Task2", 1, 8], ["Task3", 2, 3]]
+print("Priority with Round Robin Schedule:", priority_round_robin_schedule(tasks, 2))
 print("FCFS Schedule:", fcfs_schedule(tasks))
 print("SJF Schedule:", sjf_schedule(tasks))
 print("Priority Schedule:", priority_schedule(tasks))
 print("Round Robin Schedule:", round_robin_schedule(tasks, 2))
 print("Priority with Preemption Schedule:", priority_preemption_schedule(tasks))
-print("SRTF Schedule:", srtf_schedule(tasks))
